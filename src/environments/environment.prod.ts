a6815d508e868312fc7623c244c65af0f3d5bc8a
YeongCheon Kim <kyc1682@gmail.com>
2022-06-27 00:41:59 +0900

    add @angular/fire @angular/material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'PROJECT_ID',
+    appId: 'APP_ID',
+    storageBucket: 'STORAGE_BUCKET.appspot.com',
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN.firebaseapp.com',
+    messagingSenderId: 'MESSAGE_SENDER_ID',
+    measurementId: 'MEASUREMENT_ID',
+  },
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'PROJECT_ID',
+    appId: 'APP_ID',
+    storageBucket: 'STORAGE_BUCKET.appspot.com',
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN.firebaseapp.com',
+    messagingSenderId: 'MESSAGE_SENDER_ID',
+    measurementId: 'MEASUREMENT_ID',
+  },
+  production: false,
 };
 
 /*
